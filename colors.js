1db54baf1d1404a433b56364f964bebcf72ae65a
Ankeeth Suvarna <ankitsuvarna11@gmail.com>
2019-09-12 17:57:36 +0530

    Rebuilt it with Gatsby and contentful

---
+++ colors.js
@@ -0,0 +1,12 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#f0e6f6',
+
+  primary: '#4f81c7',
+  primaryLight: '#61f2f5',
+  primaryDark: '#01024e',
+
+  secondary: '#51dacf',
+  secondaryLight: '#d2fafb',
+  secondaryDark: '#41aaa8',
+};
---
+++ manifest-config.js
@@ -0,0 +1,11 @@
+const colors = require('./colors');
+
+module.exports = {
+  name: 'Ankeeth Suvarna | Software Engineer',
+  short_name: 'Ankeeth Suvarna',
+  start_url: '/',
+  background_color: colors.background,
+  theme_color: colors.primary,
+  display: 'minimal-ui',
+  icon: 'media/icon.jpeg',
+};
